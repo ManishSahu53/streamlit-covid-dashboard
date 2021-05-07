20093e1d87a183ab9045b8c24566c376b12af465
ManishSahu53 <manish.sahu@tokopedia.com>
2021-05-08 02:56:35 +0530

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+kdir -p ~/.streamlit/
+echo "[general]
+email = \"email@com\"
+" > ~/.streamlit/credentials.toml
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml

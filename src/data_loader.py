b54c0a1b0b663d7bedf4c2d0571dd886aff4cd35
Manish Sahu <manish.sahu.civ13@iitbhu.ac.in>
2021-07-22 09:32:09 +0530

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -252,8 +252,8 @@ class DataVaccineState:
         n = len(self.data)
 
         total_vaccine = self.data['Total Doses Administered'].values
-        total_covaxin = self.data['Total Covaxin Administered'].values
-        total_covidshield = self.data['Total CoviShield Administered'].values
+        total_covaxin = self.data['Covaxin (Doses Administered)'].values
+        total_covidshield = self.data['CoviShield (Doses Administered)'].values
 
         total_18_45 = self.data['18-45 years (Age)'].values
         # total_30_45 = self.data['30-45 years (Age)'].values
